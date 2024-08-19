8a245b9be8667cef5bbd86f830d05b63f6c543bd
Donny Hardyanto <3777933-donny.hardyanto@users.noreply.gitlab.com>
2024-08-19 20:44:21 +0700

    Add Webapp module and endpoint configurations
    Implemented a new module for webapp handling, including list, create, read, edit, and delete endpoints. Removed obsolete parameters and database configurations from the user access modules for mobile and webadmin services.

--- module/module.go
+++ module/module.go
@@ -1,71 +1,9 @@
 package module
 
 type DXModuleInterface interface {
-	/*	DefineConfiguration() (err error)
-		DefineAPI() (err error)
-		Start() (err error)
-		Stop() (err error)
-		DoAfterConfigurationStartAll() (err error)*/
 }
 
-/*
-	type DXInitModuleInterface interface {
-		DXModuleInterface
-		RegisterPrefixedKeyword()
-		ResolvePrefixKeyword(text string) (err error)
-	}
-*/
 type DXModule struct {
 	DXModuleInterface
 	NameId string
 }
-
-/*type DXInitModule struct {
-	DXInitModuleInterface
-	NameId        string
-	PrefixKeyword string
-}
-*/
-/*func (m *DXModule) DefineConfiguration() (err error) {
-	return nil
-}
-
-func (m *DXModule) DefineAPI() (err error) {
-	return nil
-}
-
-func (m *DXModule) Start() (err error) {
-	return nil
-}
-
-func (m *DXModule) Stop() (err error) {
-	return nil
-}
-
-func (m *DXModule) DoAfterConfigurationStartAll() (err error) {
-	return nil
-}
-
-func (im *DXInitModule) ResolvePrefixKeyword(text string) (err error) {
-	return nil
-}
-func (m *DXInitModule) DefineConfiguration() (err error) {
-	return nil
-}
-
-func (m *DXInitModule) DefineAPI() (err error) {
-	return nil
-}
-
-func (m *DXInitModule) Start() (err error) {
-	return nil
-}
-
-func (m *DXInitModule) Stop() (err error) {
-	return nil
-}
-
-func (m *DXInitModule) DoAfterConfigurationStartAll() (err error) {
-	return nil
-}
-*/
