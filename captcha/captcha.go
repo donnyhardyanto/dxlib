54caff07573a6047ced1a5f43a6c57ed106adc7c
Donny Hardyanto <hardyanto.donny@gmail.com>
2024-12-11 01:21:25 +0700

    Update dependencies and modify related modules and services
    - Update `dxlib` and `dxlib_module`.
    - Modify `service-pgn-partner-api-webadmin`.
    - Upgrade Go dependencies: CEL, Cloud SDKs, Firebase, OpenTelemetry.
    - Remove unused older dependency versions.

---
+++ captcha/captcha.go
@@ -0,0 +1,104 @@
+package captcha
+
+import (
+	"bytes"
+	"image/color"
+	"image/png"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/fogleman/gg"
+)
+
+type ICaptcha interface {
+	GenerateImage(string) ([]byte, error)
+	GenerateID() (string, string)
+}
+
+type Captcha struct {
+}
+
+// Inisialisasi random seed
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func NewCaptcha() ICaptcha {
+	return &Captcha{}
+}
+
+func (c *Captcha) GenerateID() (string, string) {
+	captchaText := generateRandomString(6)
+	captchaID := generateRandomString(20)
+
+	return captchaID, captchaText
+}
+
+func generateRandomString(length int) string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
+
+func (c *Captcha) GenerateImage(captchaText string) ([]byte, error) {
+	// Generate gambar captcha menggunakan gg (Graphics Library)
+	const width = 240
+	const height = 80
+	dc := gg.NewContext(width, height)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+
+	// Add noise (random lines) ke background
+	dc.SetColor(color.Black)
+	for i := 0; i < 15; i++ {
+		x1, y1 := rand.Float64()*width, rand.Float64()*height
+		x2, y2 := rand.Float64()*width, rand.Float64()*height
+		dc.DrawLine(x1, y1, x2, y2)
+		dc.Stroke()
+	}
+
+	// Add noise (random dots) ke background
+	for i := 0; i < 150; i++ {
+		x, y, r := rand.Float64()*width, rand.Float64()*height, rand.Float64()*20
+		//dc.SetPixel(int(x), int(y))
+		dc.DrawCircle(x, y, r)
+		dc.Fill()
+	}
+
+	if err := dc.LoadFontFace("./captcha.ttf", 52); err != nil {
+		return nil, err
+	}
+
+	startX := 70.0    // Awal posisi X untuk karakter pertama
+	yPosition := 40.0 // Posisi Y tetap sama untuk semua karakter
+
+	for i, c := range captchaText {
+		dx := math.Sin(float64(i)/2.0) * 5 // Distorsi wave horizontal
+		dy := math.Cos(float64(i)/3.0) * 5 // Distorsi wave vertical
+		// Rotate karakter lebih besar, antara -25 hingga +25 derajat
+		angle := (rand.Float64()*50 - 25) * (math.Pi / 180) // Konversi derajat ke radian
+
+		// Set rotasi dan posisi huruf dengan efek berdempetan
+		dc.RotateAbout(angle, startX+dx, yPosition+dy)
+		dc.DrawStringAnchored(string(c), startX+dx, yPosition+dy, 0.5, 0.5)
+		dc.RotateAbout(-angle, startX+dx, yPosition+dy) // Rotate kembali
+
+		// Geser startX untuk huruf berikutnya, dengan jarak yang lebih kecil (huruf lebih dempet)
+		startX += 20 // Jarak antar huruf lebih kecil dari ukuran font agar huruf tampak berdempetan
+	}
+	dc.Stroke()
+
+	var img bytes.Buffer
+	err := png.Encode(&img, dc.Image())
+	if err != nil {
+		return nil, err
+	}
+
+	return img.Bytes(), nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.3
 
 require (
 	firebase.google.com/go/v4 v4.15.0
+	github.com/fogleman/gg v1.3.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/golang-jwt/jwt/v4 v4.5.1
@@ -14,12 +15,14 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/microsoft/go-mssqldb v1.7.2
 	github.com/minio/minio-go/v7 v7.0.80
+	github.com/shopspring/decimal v1.4.0
 	github.com/sijms/go-ora/v2 v2.8.22
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.9.0
+	github.com/xuri/excelize/v2 v2.9.0
 	go.opentelemetry.io/otel v1.32.0
 	golang.org/x/crypto v0.29.0
-	golang.org/x/sync v0.9.0
+	golang.org/x/sync v0.10.0
 	golang.org/x/time v0.8.0
 	google.golang.org/api v0.205.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -58,6 +61,7 @@ require (
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
@@ -85,9 +89,7 @@ require (
 	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
-	github.com/xuri/excelize/v2 v2.9.0 // indirect
 	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.32.0 // indirect
@@ -97,10 +99,11 @@ require (
 	go.opentelemetry.io/otel/sdk v1.32.0 // indirect
 	go.opentelemetry.io/otel/sdk/metric v1.32.0 // indirect
 	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+	golang.org/x/image v0.23.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/oauth2 v0.24.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/appengine/v2 v2.0.6 // indirect
 	google.golang.org/genproto v0.0.0-20241104194629-dd2ea8efbc28 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241104194629-dd2ea8efbc28 // indirect
