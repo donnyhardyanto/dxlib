6eda061b135f9b0f6916d25f99a0091fe33cd770
donnyhardyanto <hardyanto.donny@gmail.com>
2025-03-01 09:26:50 +0700

    Add default value retrieval methods in DXVault and update API configurations to use defaults
    - Introduced `GetStringOrDefault`, `GetIntOrDefault`, `GetInt64OrDefault`, and `GetBoolOrDefault` methods in `DXHashicorpVault` to retrieve Vault values with a default fallback.
    - Added a new `VaultGetData` method for improved data extraction from Vault.
    - Updated avatar update and sub-task report picture upload APIs in `service-pgn-partner-api-mobile` to use `GetInt64OrDefault` for maximum size configuration.
    - Adjusted login rate limiter in `service-pgn-partner-api-webadmin` to streamline Vault-based configuration.
    - Minor whitespace cleanup in `service-pgn-partner-api-webadmin`.
    - Modified Docker service binaries for `service-pgn-partner-api-webadmin`, `service-pgn-partner-integration-relyon`, and `service-pgn-partner-push-notification`.

--- vault/vault.go
+++ vault/vault.go
@@ -1,7 +1,9 @@
 package vault
 
 import (
+	"fmt"
 	"github.com/donnyhardyanto/dxlib/log"
+	"github.com/donnyhardyanto/dxlib/utils"
 	vault "github.com/hashicorp/vault/api"
 	"strconv"
 	"strings"
@@ -13,6 +15,10 @@ type DXVaultInterface interface {
 	ResolveAsInt(v string) int
 	ResolveAsInt64(v string) int64
 	ResolveAsBool(v string) bool
+	GetStringOrDefault(v string, d string) string
+	GetIntOrDefault(v string, d int) int
+	GetInt64OrDefault(v string, d int64) int64
+	GetBoolOrDefault(v string, d bool) bool
 }
 
 type DXVault struct {
@@ -128,6 +134,75 @@ func (hv *DXHashicorpVault) ResolveAsString(v string) string {
 	return hv.VaultMapString(&log.Log, v)
 }
 
+func (hv *DXHashicorpVault) GetStringOrDefault(v string, d string) string {
+	data, err := hv.VaultGetData(&log.Log)
+	if err != nil {
+		panic(err)
+	}
+	dv, ok := data[v]
+	if !ok {
+		return d
+	}
+	dvv, ok := dv.(string)
+	if !ok {
+		err = fmt.Errorf("vault data is not string: %s=%v", v, dv)
+		panic(err)
+	}
+	return dvv
+}
+
+func (hv *DXHashicorpVault) GetIntOrDefault(v string, d int) int {
+	data, err := hv.VaultGetData(&log.Log)
+	if err != nil {
+		panic(err)
+	}
+	dv, ok := data[v]
+	if !ok {
+		return d
+	}
+	dvv, err := strconv.ParseInt(dv.(string), 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	return int(dvv)
+}
+
+func (hv *DXHashicorpVault) GetInt64OrDefault(v string, d int64) int64 {
+	data, err := hv.VaultGetData(&log.Log)
+	if err != nil {
+		panic(err)
+	}
+	dv, ok := data[v]
+	if !ok {
+		return d
+	}
+	dvv, err := strconv.ParseInt(dv.(string), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	return dvv
+}
+
+func (hv *DXHashicorpVault) GetBoolOrDefault(v string, d bool) bool {
+	data, err := hv.VaultGetData(&log.Log)
+	if err != nil {
+		panic(err)
+	}
+	dv, ok := data[v]
+	if !ok {
+		return d
+	}
+	dvv, err := strconv.ParseInt(dv.(string), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	if dvv == 0 {
+		return false
+	} else {
+		return true
+	}
+}
+
 func (hv *DXHashicorpVault) VaultMapping(log *log.DXLog, texts ...string) (r []string, err error) {
 	check := false
 	for _, text := range texts {
@@ -181,4 +256,18 @@ func (hv *DXHashicorpVault) VaultMapString(log *log.DXLog, text string) string {
 		return mapString
 	}
 	return text
+}
+
+func (hv *DXHashicorpVault) VaultGetData(log *log.DXLog) (r utils.JSON, err error) {
+	secret, err := hv.Client.Logical().Read(hv.Path)
+	if err != nil {
+		log.Fatalf("Unable to read credentials from Vault: %v", err.Error())
+		return nil, err
+	}
+	data, ok := secret.Data["data"].(map[string]any)
+	if !ok {
+		err = log.ErrorAndCreateErrorf("unable to read path from Vault:%s", hv.Path)
+		return nil, err
+	}
+	return data, nil
 }
