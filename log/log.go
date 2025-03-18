562a01fb76dd59511268288cc65e4a49c60306d4
donnyhardyanto <hardyanto.donny@gmail.com>
2025-03-18 22:34:04 +0700

    Refactor error handling in logging and update artifacts
    - Removed unnecessary wrapping of errors in `WarnAndCreateErrorf`, `ErrorAndCreateErrorf`, `FatalAndCreateErrorf`, and `PanicAndCreateErrorf` methods in `log.go`.
    - Changed docker artifacts:
      - `docker-service-pgn-partner-api-webadmin-linux-amd64`
      - `docker-service-pgn-partner-integration-relyon-linux-amd64`
      - `docker-service-pgn-partner-push-notification-linux-amd64`
    - Updated `dxlib` file in the library directory.

--- log/log.go
+++ log/log.go
@@ -127,7 +127,7 @@ func (l *DXLog) Warnf(text string, v ...any) {
 func (l *DXLog) WarnAndCreateErrorf(text string, v ...any) (err error) {
 	err = errors.Errorf(text, v...)
 	l.LogText(DXLogLevelWarn, ``, err.Error())
-	return errors.Wrap(err, "error occured")
+	return err
 }
 
 func (l *DXLog) Error(text string) {
@@ -142,7 +142,7 @@ func (l *DXLog) Errorf(text string, v ...any) {
 func (l *DXLog) ErrorAndCreateErrorf(text string, v ...any) (err error) {
 	err = errors.Errorf(text, v...)
 	l.Error(err.Error())
-	return errors.Wrap(err, "error occured")
+	return err
 }
 
 func (l *DXLog) Fatal(text string) {
@@ -156,7 +156,7 @@ func (l *DXLog) Fatalf(text string, v ...any) {
 func (l *DXLog) FatalAndCreateErrorf(text string, v ...any) (err error) {
 	err = errors.Errorf(text, v...)
 	l.Fatal(err.Error())
-	return errors.Wrap(err, "error occured")
+	return err
 }
 
 func (l *DXLog) Panic(location string, err error) {
@@ -166,7 +166,7 @@ func (l *DXLog) Panic(location string, err error) {
 func (l *DXLog) PanicAndCreateErrorf(location, text string, v ...any) (err error) {
 	err = errors.Errorf(text, v...)
 	l.Panic(location, err)
-	return errors.Wrap(err, "error occured")
+	return err
 }
 
 var Log DXLog
