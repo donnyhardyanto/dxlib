528ba2be5a878c4416b55b76cdb9a01d655cfed8
donnyhardyanto <hardyanto.donny@gmail.com>
2025-06-19 15:10:45 +0700

    Fixed incorrect log reference to `NonSensitiveConnectionString` and added additional logging for database configuration status.

--- database/database.go
+++ database/database.go
@@ -172,6 +172,7 @@ func (d *DXDatabase) GetConnectionString() (s string, err error) {
 	default:
 		err = log.Log.ErrorAndCreateErrorf("configuration is unusable, value of database_type field of database %s configuration is not supported (%s)", d.NameId, s)
 	}
+	log.Log.Info(s)
 	return s, err
 }
 
@@ -274,7 +275,7 @@ func (d *DXDatabase) ApplyFromConfiguration() (err error) {
 		if err != nil {
 			return err
 		}
-		log.Log.Infof("Connecting to Database %s... done", d.ConnectionString)
+		log.Log.Infof("Connecting to Database %s... done", d.NonSensitiveConnectionString)
 		d.IsConfigured = true
 		log.Log.Infof("Configuring to Database %s... done", d.NameId)
 	}
